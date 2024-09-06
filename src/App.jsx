40b5a149874ea5a19c51d3a47b16f663db8051c0
aryan-bhargav <bhargavaryan5@gmail.com>
2024-09-06 15:24:36 +0530

    react-router

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Home/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
